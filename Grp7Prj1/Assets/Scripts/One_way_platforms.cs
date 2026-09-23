332b3f51ed0489bb2d64ca0064533d6f0f0d9347
VTCode-hub <Victor.tranberg@gmail.com>
2026-09-23 23:25:34 +0200

    one way platforms

---
+++ Grp7Prj1/Assets/Scripts/One_way_platforms.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class One_way_platforms : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+
+
+
+        }
+    }
+}
